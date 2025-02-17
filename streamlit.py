45a97977316f25f4eb3a41d16d387ca4bb65b666
Hasin Jawad Ali <alij85791@gmail.com>
2025-02-17 09:39:18 +0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -27,7 +27,7 @@ from googleapiclient.http import MediaFileUpload
 import traceback
 
 
-load_dotenv(dotenv_path= '/teamspace/studios/this_studio/env.txt')
+load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  
 
 GROQ= os.getenv("GROQ_API_KEY")
